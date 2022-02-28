51b6ce4b6b375d1c6b9c03fd2c26d3ce9108cfef
marciadev <marcialeite483@gmail.com>
2022-02-27 23:43:29 -0300

    Form create

--- client/src/components/CreateVideogame.jsx
+++ client/src/components/CreateVideogame.jsx
@@ -5,12 +5,40 @@ import axios from "axios";
 import Select from "react-select";
 import { Link, useHistory } from "react-router-dom";
 
+
+export function validate(newVideogame) {
+  let errors = {};
+  if (!newVideogame.name) {
+    errors.name = "You need to introduce a name";
+  } else if (!/^[^\W0-9_][a-zA-Z0-9\s]+$/.test(newVideogame.name)){
+    errors.name = "Invalid name";
+  }
+  if (!newVideogame.rating || newVideogame.rating > 5){
+   errors.rating = "Rating must be a number between 1-5"
+  }
+  if (!newVideogame.imageUrl){
+    errors.imageUrl = "Introduce a pic in format jpg, jpeg or png"
+  }
+  if (!newVideogame.description){
+    errors.description = "Describe your videogame"
+  }
+  // if(newVideogame.platforms.length === 0) {
+  //   errors.platforms = "Choose the platforms"
+  // }
+  // if(!newVideogame.genres) {
+  //   errors.genres = "Choose the genres"
+  // }
+  return errors;
+}
+
 export function CreateVideogame() {
   const history = useHistory();
   const dispatch = useDispatch();
   const stateGenre = useSelector((state) => state.genres);
   const platforms = ["PC", "PlayStation 3", "PlayStation 4", "PlayStation 5", "macOS", "Linux", "Xbox360", "Android", "Nintendo Switch", "iOS", "Xbox One", "Xbox Series S/X"]
   
+  const [errors, setErrors] = useState({})
+
   const [newVideogame, setNewVideogame] = useState({
     name: "",
     releaseDate: "",
@@ -60,6 +88,10 @@ export function CreateVideogame() {
       ...newVideogame,
       [e.target.name]: e.target.value,
     });
+    setErrors(validate({
+      ...newVideogame,
+      [e.target.name] : e.target.value
+    }))
   };
 
   const handleSelect = (e) => {
@@ -88,22 +120,27 @@ export function CreateVideogame() {
         <div>
         <label>Name:</label>
         <input name="name" value={newVideogame.name} onChange={handleChange} />
+        {errors.name}
         </div>
         <div>
         <label>Release Date:</label>
         <input
+        type='date'
           name="releaseDate"
           value={newVideogame.releaseDate}
           onChange={handleChange}
         />
+        {errors.releaseDate}
         </div>
         <div>
         <label>Rating:</label>
         <input
+        type='number' min='0' max='5'
           name="rating"
           value={newVideogame.rating}
           onChange={handleChange}
         />
+        {errors.rating}
         </div>
         <div>
         <label>ImageURL:</label>
@@ -112,6 +149,7 @@ export function CreateVideogame() {
           value={newVideogame.imageUrl}
           onChange={handleChange}
         />
+        {errors.imageUrl}
         </div>
         <div>
         <label>Description:</label>
@@ -120,6 +158,7 @@ export function CreateVideogame() {
           value={newVideogame.description}
           onChange={handleChange}
         />
+        {errors.description}
         </div>
         <div>
         <label>Platforms:</label>
@@ -129,7 +168,7 @@ export function CreateVideogame() {
         <label>Genres:</label>
          <Select isMulti options={genreOptions} onChange={(e)=>{handleSelect(e)}}/> 
         </div>
-        <button type="submit">Create</button>
+        <button type="submit" disabled={errors.name || errors.releaseDate || errors.imageUrl || errors.rating || errors.description ? true : false}>Create</button>
       </form>
     </div>
   );
